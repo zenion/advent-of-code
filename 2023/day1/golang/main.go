23701e390dcc1683c8fcef01fea25a8dce85d0d7
zenion <joshstout@gmail.com>
2024-12-04 22:18:14 +0800

    filling in previous stuff

---
+++ 2023/day1/golang/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	content, err := os.ReadFile("./input.txt")
+	if err != nil {
+		panic(err)
+	}
+	lines := strings.Split(string(content), "\n")
+
+	// strip empty lines
+	lines = slices.DeleteFunc(lines, func(line string) bool {
+		return line == ""
+	})
+
+	// part 1
+	sumPart1 := 0
+	for _, line := range lines {
+		var digits []string
+
+		// Check each position in the string for digits
+		for i := 0; i < len(line); i++ {
+			if line[i] >= '0' && line[i] <= '9' {
+				digits = append(digits, string(line[i]))
+			}
+		}
+
+		if len(digits) == 0 {
+			continue
+		}
+
+		// Take first and last digit
+		first := digits[0]
+		last := digits[len(digits)-1]
+
+		num, err := strconv.Atoi(first + last)
+		if err != nil {
+			panic(err)
+		}
+		sumPart1 += num
+	}
+
+	// part 2
+	stringToNumber := map[string]string{
+		"one":   "1",
+		"two":   "2",
+		"three": "3",
+		"four":  "4",
+		"five":  "5",
+		"six":   "6",
+		"seven": "7",
+		"eight": "8",
+		"nine":  "9",
+	}
+
+	sumPart2 := 0
+	for _, line := range lines {
+		var digits []string
+
+		// Check each position in the string for either a digit or a word
+		for i := 0; i < len(line); i++ {
+			// Check for numeric digit
+			if line[i] >= '0' && line[i] <= '9' {
+				digits = append(digits, string(line[i]))
+				continue
+			}
+
+			// Check for word numbers
+			for word, num := range stringToNumber {
+				if strings.HasPrefix(line[i:], word) {
+					digits = append(digits, num)
+				}
+			}
+		}
+
+		if len(digits) == 0 {
+			continue
+		}
+
+		// Take first and last digit
+		first := digits[0]
+		last := digits[len(digits)-1]
+
+		num, err := strconv.Atoi(first + last)
+		if err != nil {
+			panic(err)
+		}
+		sumPart2 += num
+	}
+
+	fmt.Println(sumPart1)
+	fmt.Println(sumPart2)
+}
--- 2024/day1/golang/main.go
+++ 2024/day1/golang/main.go
@@ -2,12 +2,80 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"math"
 	"os"
+	"slices"
+	"strconv"
+	"strings"
 )
 
-func main() {
-	file_data, _ := os.ReadFile("input.txt")
-	fmt.Println(string(file_data))
+func readNumberPairs(filePath string) ([]int, []int, error) {
+	content, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, nil, err
+	}
 
-	fmt.Println("Seasons greetings gamers!")
+	lines := strings.Split(string(content), "\n")
+
+	// Remove any empty lines
+	lines = slices.DeleteFunc(lines, func(line string) bool {
+		return line == ""
+	})
+
+	var left, right []int
+	for _, line := range lines {
+		nums := strings.Split(line, "   ")
+
+		numA, numB := 0, 0
+		if numA, err = strconv.Atoi(nums[0]); err != nil {
+			return nil, nil, err
+		}
+		if numB, err = strconv.Atoi(nums[1]); err != nil {
+			return nil, nil, err
+		}
+
+		left = append(left, numA)
+		right = append(right, numB)
+	}
+
+	return left, right, nil
+}
+
+func calculateAbsoluteDifferences(left, right []int) int {
+	slices.Sort(left)
+	slices.Sort(right)
+
+	sum := 0
+	for i := 0; i < len(left); i++ {
+		sum += int(math.Abs(float64(left[i] - right[i])))
+	}
+	return sum
+}
+
+func calculateMatchingProducts(left, right []int) int {
+	sum := 0
+	for i := 0; i < len(left); i++ {
+		countInRight := 0
+		for j := 0; j < len(right); j++ {
+			if left[i] == right[j] {
+				countInRight++
+			}
+		}
+		sum += left[i] * countInRight
+	}
+	return sum
+}
+
+func main() {
+	left, right, err := readNumberPairs("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	part1 := calculateAbsoluteDifferences(left, right)
+	part2 := calculateMatchingProducts(left, right)
+
+	fmt.Printf("Part 1: %d\n", part1)
+	fmt.Printf("Part 2: %d\n", part2)
 }
