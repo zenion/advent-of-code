baca3edb5780ba560ecf8bf814b545a1420959b7
msew <7599480+msew@users.noreply.github.com>
2024-12-16 12:10:30 +0800

    Day 9 (part 1)

--- 2024/day9/golang/main.go
+++ 2024/day9/golang/main.go
@@ -2,12 +2,66 @@ package main
 
 import (
 	"fmt"
-	"os"
+
+	aoc "github.com/zenion/advent-of-code"
 )
 
 func main() {
-	file_data, _ := os.ReadFile("input.txt")
-	fmt.Println(string(file_data))
+	fileLines := aoc.ReadFileLines("input.txt")
+	diskMap := aoc.MapFunc([]byte(fileLines[0]), func(b byte) int {
+		return aoc.AtoiNoError(string(b))
+	})
 
-	fmt.Println("Seasons greetings gamers!")
+	blocks := calcBlocks(diskMap)
+
+	compact(blocks)
+	checksum := calcChecksum(blocks)
+
+	fmt.Println(checksum)
+}
+
+func calcChecksum(blocks []int) int {
+	checksum := 0
+	for i := 0; i < len(blocks); i++ {
+		if blocks[i] >= 0 {
+			checksum += i * blocks[i]
+		}
+	}
+	return checksum
+}
+
+func calcBlocks(diskMap []int) []int {
+	blocks := make([]int, 0, 2*len(diskMap))
+	id := 0
+	empty := false
+	for i := 0; i < len(diskMap); i++ {
+		for j := 0; j < diskMap[i]; j++ {
+			if empty {
+				blocks = append(blocks, -1)
+			} else {
+				blocks = append(blocks, id)
+			}
+		}
+		if !empty {
+			id++
+		}
+		empty = !empty
+	}
+	return blocks
+}
+
+func compact(blocks []int) {
+	left := 0
+	right := len(blocks) - 1
+	for left < right {
+		if blocks[right] < 0 {
+			right--
+		} else if blocks[left] < 0 {
+			blocks[left] = blocks[right]
+			blocks[right] = -1
+			left++
+		} else {
+			left++
+		}
+	}
 }
