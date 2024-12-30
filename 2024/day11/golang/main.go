5c1a7ef22d69d08098231ce575cdd4bbeb6c6adf
msew <7599480+msew@users.noreply.github.com>
2024-12-31 02:24:05 +0800

    Day 11 (part 1)

--- 2024/day11/golang/main.go
+++ 2024/day11/golang/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	aoc "github.com/zenion/advent-of-code"
 )
@@ -9,7 +11,29 @@ import (
 func main() {
 	fileLines := aoc.ReadFileLines("input.txt")
 
-	for _, s := range fileLines {
-		fmt.Println(s)
+	stones := aoc.MapFunc(strings.Split(fileLines[0], " "), aoc.AtoiNoError)
+
+	for i := 0; i < 25; i++ {
+		stones = blink(stones)
 	}
+
+	fmt.Println(len(stones))
+}
+
+func blink(stones []int) []int {
+	result := make([]int, 0, len(stones))
+
+	for _, s := range stones {
+		if s == 0 {
+			result = append(result, 1)
+		} else if len(strconv.Itoa(s))%2 == 0 {
+			str := strconv.Itoa(s)
+			result = append(result, aoc.AtoiNoError(str[0:len(str)/2]))
+			result = append(result, aoc.AtoiNoError(str[len(str)/2:]))
+		} else {
+			result = append(result, s*2024)
+		}
+	}
+
+	return result
 }
